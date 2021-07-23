8785c591a2859642d176fa473a6da46186bec3db
Anton Kornienko <anton.kornienko74@mail.ru>
2021-07-23 23:47:20 +0300

    Added cpu model

---
+++ src/Chip8/Chip8.Core/Contracts/ICpu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8.Core.Contracts
+{
+    public interface ICpu
+    {
+        int[] Resgisters { get; }
+        int SP { get; set; }
+        int PC { get; set; }
+        int I { get; set; }
+        bool VF { get; set; }
+        int[] Stack { get; }
+    }
+}
---
+++ src/Chip8/Chip8.Internal/Services/Cpu.cs
@@ -0,0 +1,21 @@
+﻿using Chip8.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8.Internal.Services
+{
+    public class Cpu : ICpu
+    {
+        public int[] Resgisters { get; } = new int[16];
+
+        public int SP { get; set; }
+        public int PC { get; set; }
+        public int I { get; set; }
+        public bool VF { get; set; }
+
+        public int[] Stack { get; } = new int[16];
+    }
+}
